99615432b9047577367454f408f7bfc6f094e4cc
Jason Orendorff <jason.orendorff@gmail.com>
2017-07-26 08:58:55 -0500

    lisp: Refactor benchmark code (to no real effect).

--- lisp/benches/bench.rs
+++ lisp/benches/bench.rs
@@ -4,33 +4,37 @@ extern crate cell_gc;
 extern crate lisp;
 extern crate test;
 
-use lisp::{parse, vm};
-use std::fs;
-use std::io::Read;
+use lisp::parse;
+use lisp::vm::{self, Environment};
 
 macro_rules! bench {
     ( $filename:ident ) => {
         #[bench]
         fn $filename(b: &mut test::Bencher) {
-            let filename = concat!(env!("CARGO_MANIFEST_DIR"),
-                                   "/benches/",
-                                   stringify!($filename),
-                                   ".scm");
-            let mut file = fs::File::open(filename).expect("should open file OK");
-            let mut source = String::new();
-            file.read_to_string(&mut source).expect("should read file OK");
-
-            b.iter(|| {
-                cell_gc::with_heap(|hs| {
-                    let env = vm::Environment::default_env(hs);
-                    let exprs = parse::parse(hs, &source).expect("should parse source OK");
-                    for expr in exprs {
-                        vm::eval(hs, expr, env.clone()).expect("should eval OK");
-                    }
-                });
-            });
+            run_benchmark(
+                b,
+                include_str!(
+                    concat!(env!("CARGO_MANIFEST_DIR"),
+                            "/benches/",
+                            stringify!($filename),
+                            ".scm")
+                ),
+            );
         }
     }
+}
+
+fn run_benchmark(b: &mut test::Bencher, code: &str) {
+    b.iter(|| {
+        let mut heap = cell_gc::GcHeap::new();
+        heap.enter(|hs| {
+            let env = Environment::default_env(hs);
+            let exprs = parse::parse(hs, code).expect("should parse source OK");
+            for expr in exprs {
+                vm::eval(hs, expr, env.clone()).expect("should eval OK");
+            }
+        });
+    });
 }
 
 bench!(allocate_no_garbage);
