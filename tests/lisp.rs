caa463bf9b7c83ca9b8b39de7a14b63a80fa7e65
Jason Orendorff <jason.orendorff@gmail.com>
2017-08-02 05:21:39 -0500

    In optimized builds, run lisp tests with `cargo test --release`.
    They are annoyingly slow otherwise.

--- tests/lisp.rs
+++ tests/lisp.rs
@@ -2,8 +2,14 @@ use std::process::Command;
 
 #[test]
 fn run_lisp_tests() {
+    let mut args = vec!["test"];
+
+    if !cfg!(debug_assertions) {
+        args.push("--release");
+    }
+
     let status = Command::new("cargo")
-        .args(&["test"])
+        .args(&args)
         .current_dir(concat!(env!("CARGO_MANIFEST_DIR"), "/lisp"))
         .spawn()
         .expect("should spawn tests OK")
