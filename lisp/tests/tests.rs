782df9be0c4927ad57e2b2dd16aa9e3b4d103ebc
Jason Orendorff <jason.orendorff@gmail.com>
2017-08-18 10:03:12 -0500

    lisp: Add r5rs pitfall test.

--- lisp/tests/tests.rs
+++ lisp/tests/tests.rs
@@ -5,7 +5,13 @@ use std::fs;
 use std::io::Read;
 use std::path::Path;
 
-fn eval_test_file(expanded: bool, file: &Path) {
+enum Mode {
+    Core,
+    Expanded,
+    Full,
+}
+
+fn eval_test_file(mode: Mode, file: &Path) {
     let mut file = fs::File::open(file).expect("Should open file OK");
 
     let mut source = String::new();
@@ -14,10 +20,10 @@ fn eval_test_file(expanded: bool, file: &Path) {
 
     cell_gc::with_heap(|hs| {
         let exprs = lisp::parse::parse(hs, &source).expect("Should parse s-exps OK");
-        let env = if expanded {
-            lisp::toplevel::expanded_env(hs)
-        } else {
-            lisp::toplevel::core_env(hs)
+        let env = match mode {
+            Mode::Core => lisp::toplevel::core_env(hs),
+            Mode::Expanded => lisp::toplevel::expanded_env(hs),
+            Mode::Full => lisp::toplevel::default_env(hs),
         };
         for expr in exprs {
             lisp::toplevel::eval(hs, &env, expr).expect("Should eval exprs OK");
@@ -26,44 +32,37 @@ fn eval_test_file(expanded: bool, file: &Path) {
     });
 }
 
-macro_rules! expanded_test {
-    ( $file:ident ) => {
+macro_rules! test {
+    ( $kind:ident, $file:ident ) => {
         #[test]
         fn $file() {
             let file = concat!(env!("CARGO_MANIFEST_DIR"), "/tests/", stringify!($file), ".scm");
-            eval_test_file(true, Path::new(file));
+            eval_test_file(Mode::$kind, Path::new(file));
         }
     }
 }
 
-macro_rules! core_test {
-    ( $file:ident ) => {
-        #[test]
-        fn $file() {
-            let file = concat!(env!("CARGO_MANIFEST_DIR"), "/tests/", stringify!($file), ".scm");
-            eval_test_file(false, Path::new(file));
-        }
-    }
-}
 
-core_test!(begin);
-core_test!(booleans);
-core_test!(characters);
-core_test!(closure);
-core_test!(cons);
-expanded_test!(continuations);
-core_test!(double);
-expanded_test!(equivalence);
-expanded_test!(eval);
-core_test!(extensions);
-core_test!(y_combinator);
-core_test!(y_combinator_2);
-core_test!(define);
-core_test!(numerical);
-expanded_test!(pairs_and_lists);
-expanded_test!(primitive);
-core_test!(print);
-core_test!(set);
-core_test!(symbols);
-core_test!(tail_calls);
-core_test!(vectors);
+test!(Core, begin);
+test!(Core, booleans);
+test!(Core, characters);
+test!(Core, closure);
+test!(Core, cons);
+test!(Expanded, continuations);
+test!(Core, double);
+test!(Expanded, equivalence);
+test!(Full, eval);
+test!(Core, extensions);
+test!(Core, y_combinator);
+test!(Core, y_combinator_2);
+test!(Core, define);
+test!(Core, numerical);
+test!(Expanded, pairs_and_lists);
+test!(Expanded, primitive);
+test!(Core, print);
+test!(Core, set);
+test!(Core, symbols);
+test!(Core, tail_calls);
+test!(Core, vectors);
+
+test!(Full, r5rs_pitfall);
